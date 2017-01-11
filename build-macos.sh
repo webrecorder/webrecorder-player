bcc2863e141dd0ab6a39a498f3ce3cf53ae99865
raffaele messuti <raffaele@atomotic.com>
2017-01-11 12:46:28 +0100

    fix build script for webrecorder standalone

--- build-macos.sh
+++ build-macos.sh
@@ -3,17 +3,19 @@
 mkdir build
 cd build
 
+printf "\e[91m# create virtualenv\e[0m\n"
 virtualenv -p python3.5 venv && source venv/bin/activate
-which python
 
+printf "\e[91m# clone webrecorder@develop\e[0m\n"
 git clone https://github.com/webrecorder/webrecorder.git
 cd webrecorder
 git fetch
 git checkout origin/develop
 
+printf "\e[91m# build\e[0m\n"
 cd webrecorder
+pip install Jinja2==2.8 # latest Jinja breaks Pyinstaller!
 python setup.py install
 ./webrecorder/standalone/build.sh
 cp webrecorder/standalone/dist/standalone ../../../python-binaries/webrecorder
-
 rm -fr build
