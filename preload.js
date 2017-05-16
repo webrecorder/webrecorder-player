94d860ffa5452fc9c9ac0f3c3150fe3d7859abb7
Mark Beasley <mbeasl1@gmail.com>
2017-05-16 12:30:28 -0400

    Add iframeLoad function to catch subframe load events

--- preload.js
+++ preload.js
@@ -7,3 +7,8 @@ global.sendToHost = (msg, data) => {
 ipcRenderer.on("version", (evt, arg) => {
   document.getElementById("stack-version").innerHTML= arg;
 });
+
+
+global.iframeLoad = () => {
+  ipcRenderer.sendToHost('did-frame-navigate');
+}
