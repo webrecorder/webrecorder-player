a4512e3c559959298132a95dcb9ef0cd5e4876c7
mark f beasley <mbeasl1@gmail.com>
2017-05-18 19:35:41 -0400

    History fix (#30)
    * Add iframeLoad function to catch subframe load events
    
    * Add pywb flag, use goToIndex to avoid issue with first history change

--- preload.js
+++ preload.js
@@ -7,3 +7,9 @@ global.sendToHost = (msg, data) => {
 ipcRenderer.on("version", (evt, arg) => {
   document.getElementById("stack-version").innerHTML= arg;
 });
+
+global.pushStateOnLoad = true;
+
+global.iframeLoad = () => {
+  ipcRenderer.sendToHost("did-frame-navigate");
+}
