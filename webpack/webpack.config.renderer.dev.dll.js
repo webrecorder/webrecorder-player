fd7110107138cbdf7d4e34842078274cca23d257
Mark Beasley <mbeasl1@gmail.com>
2017-11-07 15:01:15 -0500

    Fix build error - Fixes superagent build error

--- webpack/webpack.config.renderer.dev.dll.js
+++ webpack/webpack.config.renderer.dev.dll.js
@@ -193,7 +193,8 @@ export default merge.smart(baseConfig, {
      * development checks
      */
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      "global.GENTLY": false
     }),
 
     new webpack.LoaderOptionsPlugin({
