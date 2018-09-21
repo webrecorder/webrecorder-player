75d872eddf11fe5613f7c928d2b2814850affb0b
Ilya Kreymer <ikreymer@gmail.com>
2018-09-21 16:56:25 -0700

    build-wr: update build-wr.sh script to run python3 setup.py install, exit on errors

--- build-wr.sh
+++ build-wr.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-bash ./webrecorder/webrecorder/webrecorder/standalone/build_player.sh 
+set -e
+
+pushd webrecorder/webrecorder
+python3 setup.py install
+bash ./webrecorder/standalone/build_player.sh
+popd
+
 mv ./webrecorder/webrecorder/webrecorder/standalone/dist/webrecorder_player ./python-binaries/
 chmod a+x ./python-binaries/webrecorder_player
 
